302ef82f3c1627e12f0a006677fc401e9c412275
danangsugiarto <danangsugiartotkj5@gmail.com>
2022-08-28 04:48:59 +0700

    refactor(controller): remove unused controllers

--- src/controllers/BotCommandController.js
+++
@@ -1,7 +0,0 @@
-const axios = require('axios');
-
-const conversionHandler = async (from, to, value) => {
-    return `Convert from ${from} to ${to} ${value}`;
-}
-
-module.exports = { conversionHandler };
